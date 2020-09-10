95ab1fa8b88b0c9316b8d2282f8c5ae0215d803e
Lipx <1261203542@qq.com>
2020-09-10 23:58:36 +0800

    支持setState

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import { createElement, render, Component } from './toy-react'
 
 class MyComponent extends Component {
+    constructor () {
+        super()
+        this.state = {
+            a: 1,
+            b: 2
+        }
+    }
     render () {
         return <div>
             <h1>my component</h1>
-            {this.children}
+            <button onclick={ () => { this.setState({ a: this.state.a + 1 }) } }>add</button>
+            <span>{this.state.a.toString()}</span>
+            <span>{this.state.b.toString()}</span>
         </div>
     }
 }
--- toy-react.js
+++ toy-react.js
@@ -1,12 +1,25 @@
+const RENDER_TO_DOM = Symbol('render to dom')
+
 class ElementWrapper {
     constructor (type) {
         this.root = document.createElement(type)
     }
     setAttribute (name, value) {
-        this.root.setAttribute(name, value)
+        if (name.match(/^on([\s\S]+)$/)) {
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+        } else {
+            this.root.setAttribute(name, value)
+        }
     }
     appendChild (component) {
-        this.root.appendChild(component.root)
+        let range = document.createRange()
+        range.setStart(this.root, this.root.childNodes.length)
+        range.setEnd(this.root, this.root.childNodes.length)
+        component[RENDER_TO_DOM](range)
+    }
+    [RENDER_TO_DOM] (range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
@@ -14,13 +27,17 @@ class TextWrapper {
     constructor (content) {
         this.root = document.createTextNode(content)
     }
+    [RENDER_TO_DOM] (range) {
+        range.deleteContents()
+        range.insertNode(this.root)
+    }
 }
 
 export class Component {
     constructor () {
         this.props = Object.create(null)
         this.children = []
-        this._root = null
+        this._range = null
     }
     setAttribute (name, value) {
         this.props[name] = value
@@ -28,11 +45,32 @@ export class Component {
     appendChild (component) {
         this.children.push(component)
     }
-    get root () {
-        if (!this._root) {
-            this._root = this.render().root
+    [RENDER_TO_DOM] (range) {
+        this._range = range
+        this.render()[RENDER_TO_DOM](range)
+    }
+    rerender () {
+        this._range.deleteContents()
+        this[RENDER_TO_DOM](this._range)
+    }
+    setState (newState) {
+        if (this.state === null || typeof this.state !== 'object') {
+            this.state = newState
+            this.rerender()
+            return
         }
-        return this._root
+
+        let merge = (oldState, newState) => {
+            for (let k in newState) {
+                if (oldState[k] === null || typeof oldState[k] !== 'object') {
+                    oldState[k] = newState[k]
+                } else {
+                    merge(oldState[k], newState[k])
+                }
+            }
+        }
+        merge(this.state, newState)
+        this.rerender()
     }
 }
 
@@ -63,5 +101,9 @@ export function createElement (type, attrMap, ...children) {
 }
 
 export function render (component, parentElement) {
-    parentElement.appendChild(component.root)
+    let range = document.createRange()
+    range.setStart(parentElement, 0)
+    range.setEnd(parentElement, parentElement.childNodes.length)
+    range.deleteContents()
+    component[RENDER_TO_DOM](range)
 }
